5b9563f131bf4135fc16f34154ad09b56eef009b
Kensei <tehkensei@gmail.com>
2025-01-10 15:02:57 -0600

    initial

---
+++ least.rb
@@ -0,0 +1,19 @@
+
+class Least < Formula
+  desc "A Lightweight less Alternative for Code Browsing"
+  homepage "https://github.com/kenseitehdev/Least"
+  url "https://github.com/kenseitehdev/Least/releases/download/0.2/least-0.2.tar.gz"
+  sha256 "7bd0174d90a33de76cb4554506dc593a4a5844f3e6fec5b6cb881b158b22a66f"
+  license "MIT"
+
+  depends_on "ncurses"
+
+  def install
+    system "make"
+    bin.install "least"
+  end
+
+  test do
+    system "#{bin}/least", "--version"
+  end
+end
